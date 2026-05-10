fe789d8fa7b8fc456196971af9b255464f5a007b
chamitdagar <amit.brandmee@gmail.com>
2026-05-10 23:45:14 +0530

    Fix static file path routing to bypass Nginx limitations

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use((req, res, next) => {
 });
 app.use(express.json());
 app.use(cookieParser());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/api/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
 app.use('/api/auth', authRoutes);
